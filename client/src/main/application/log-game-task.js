4cae2434e6e857aab0d8198c097c77f1c88892a8
unknown <f_roseberry@live.fr>
2015-08-18 19:41:48 -0400

    Changed the dice roll log message for a double

--- client/src/main/application/log-game-task.js
+++ client/src/main/application/log-game-task.js
@@ -16,20 +16,35 @@
 	}
 	
 	function watchGame(messages, playGameTask) {
-		playGameTask.rollDiceTaskCreated().subscribe(function (task) {
-			task.diceRolled().last().withLatestFrom(
-				playGameTask.gameState(),
-				function (dice, state) {
-					return {
-						firstDie : dice[0],
-						secondDie: dice[1],
-						player: state.players[state.currentPlayerIndex].name
-					};
-				})
-				.subscribe(function (dice) {
-					messages.onNext(dice.player + ' rolled a ' + dice.firstDie + ' and a ' + dice.secondDie);
-				});
-		});
+		onDiceRolled(playGameTask)
+			.subscribe(function (dice) {
+				messages.onNext(dice.player + ' rolled ' + diceMessage(dice));
+			});
+	}
+	
+	function onDiceRolled(playGameTask) {
+		return playGameTask.rollDiceTaskCreated()
+			.flatMap(function (task) {
+				return task.diceRolled().last().withLatestFrom(
+					playGameTask.gameState(),
+					combineDiceAndState);
+			});
+	}
+	
+	function combineDiceAndState(dice, state) {
+		return {
+			firstDie : dice[0],
+			secondDie: dice[1],
+			player: state.players[state.currentPlayerIndex].name
+		};
+	}
+	
+	function diceMessage(dice) {
+		if (dice.firstDie === dice.secondDie) {
+			return 'a double of ' + dice.firstDie;
+		}
+		
+		return 'a ' + dice.firstDie + ' and a ' + dice.secondDie;		
 	}
 	
 	LogGameTask.prototype.messages = function () {
